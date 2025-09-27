fdb37661c92a42767bc7f9af256ed147bf3cc7ad
kshama-jay247 <kshamajaychand@gmail>
2025-09-27 14:12:01 +0530

    Landing Page

--- src/Pages/Landing_page.jsx
+++ src/Pages/Landing_page.jsx
@@ -1,8 +1,11 @@
+import { useState } from 'react'
+import '../Pages Styling/Landing_page.css';
+import LiquidEther from '../assets/bg1.jsx'
 
 export function LandingPage() {
-    return (
-        <>
-
-        </>
-    );
+  return (
+    <>
+     <LiquidEther/>
+    </>
+  )
 }
